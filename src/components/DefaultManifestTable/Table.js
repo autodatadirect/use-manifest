e8b9237b070b54222a265e49126dca83f01233c6
Charles Howard <cdhoward@add123.com>
2020-03-11 09:33:34 -0400

    Table props (#6) adding methods to handle adding optional TR and TD props

--- src/components/DefaultManifestTable/Table.js
+++ src/components/DefaultManifestTable/Table.js
@@ -3,57 +3,89 @@ import PropTypes from 'prop-types'
 import HeaderCell from './HeaderCell'
 import Cell from './DataCell'
 import integerSequence from '../../utils/integerSequence'
+import useManifest from '../../hooks/useManifest'
+import useCell from '../../hooks/useCell'
 
-const Table = ({ className, columnCount, rowCount }) => {
-  const columnIndexs = useMemo(() => integerSequence(columnCount), [columnCount])
+const EMPTY_PROPS = {}
+
+const Table = ({ className, columnCount, rowCount, trPropsHandler, tdPropsHandler }) => {
+  const columnIndexes = useMemo(() => integerSequence(columnCount), [columnCount])
 
   return (
     <table className={`manifest-table ${className || ''}`}>
-      <Headers columnIndexs={columnIndexs} />
-      <Rows rowCount={rowCount} columnIndexs={columnIndexs} />
+      <Headers columnIndexes={columnIndexes} />
+      <Rows rowCount={rowCount} columnIndexes={columnIndexes} trPropsHandler={trPropsHandler} tdPropsHandler={tdPropsHandler} />
     </table>
   )
 }
 
 Table.propTypes = {
   columnCount: PropTypes.number.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  trPropsHandler: PropTypes.func.isRequired,
+  tdPropsHandler: PropTypes.func.isRequired
 }
 
-const Headers = ({ columnIndexs }) =>
+const Headers = ({ columnIndexes }) =>
   <thead>
     <tr>
-      {columnIndexs.map(i => <th key={i}><HeaderCell columnIndex={i} /></th>)}
+      {columnIndexes.map(i => <th key={i}><HeaderCell columnIndex={i} /></th>)}
     </tr>
   </thead>
 
 Headers.propTypes = {
-  columnIndexs: PropTypes.array.isRequired
+  columnIndexes: PropTypes.array.isRequired
 }
 
-const Rows = ({ rowCount, columnIndexs }) => {
+const Rows = ({ rowCount, columnIndexes, trPropsHandler, tdPropsHandler }) => {
   const rowIndexs = useMemo(() => integerSequence(rowCount), [rowCount])
 
   return (
     <tbody>
-      {rowIndexs.map(i => <Row key={i} rowIndex={i} columnIndexs={columnIndexs} />)}
+      {rowIndexs.map(i => <TableRow key={i} rowIndex={i} columnIndexes={columnIndexes} trPropsHandler={trPropsHandler} tdPropsHandler={tdPropsHandler} />)}
     </tbody>
   )
 }
 
 Rows.propTypes = {
   rowCount: PropTypes.number.isRequired,
-  columnIndexs: PropTypes.array.isRequired
+  columnIndexes: PropTypes.array.isRequired,
+  trPropsHandler: PropTypes.func.isRequired,
+  tdPropsHandler: PropTypes.func.isRequired
 }
 
-const Row = ({ rowIndex, columnIndexs }) =>
-  <tr>
-    {columnIndexs.map(columnIndex => <td key={columnIndex + '.' + rowIndex}><Cell columnIndex={columnIndex} rowIndex={rowIndex} /></td>)}
-  </tr>
+const TableRow = ({ rowIndex, columnIndexes, trPropsHandler, tdPropsHandler }) => {
+  const state = useManifest()
+  const row = state.rows[rowIndex]
+  const props = trPropsHandler({ rowIndex, row }) || EMPTY_PROPS
+  return (
+    <tr {...props}>
+      {columnIndexes.map(columnIndex => <TableData key={columnIndex} rowIndex={rowIndex} columnIndex={columnIndex} tdPropsHandler={tdPropsHandler} />)}
+    </tr>
+  )
+}
 
-Row.propTypes = {
+TableRow.propTypes = {
   rowIndex: PropTypes.number.isRequired,
-  columnIndexs: PropTypes.array.isRequired
+  columnIndexes: PropTypes.array.isRequired,
+  trPropsHandler: PropTypes.func.isRequired,
+  tdPropsHandler: PropTypes.func.isRequired
+}
+
+const TableData = ({ rowIndex, columnIndex, tdPropsHandler }) => {
+  const { row, id, label, value, def, sorts } = useCell({ rowIndex, columnIndex })
+  const props = tdPropsHandler({ rowIndex, columnIndex, row, id, label, value, def, sorts }) || EMPTY_PROPS
+  return (
+    <td {...props} key={columnIndex + '.' + rowIndex}>
+      <Cell columnIndex={columnIndex} rowIndex={rowIndex} />
+    </td>
+  )
+}
+
+TableData.propTypes = {
+  rowIndex: PropTypes.number.isRequired,
+  columnIndexes: PropTypes.array.isRequired,
+  tdPropsHandler: PropTypes.func.isRequired
 }
 
 export default Table
--- src/components/DefaultManifestTable/index.js
+++ src/components/DefaultManifestTable/index.js
@@ -4,25 +4,30 @@ import Table from './Table'
 import Controls from './Controls'
 import useManifest from '../../hooks/useManifest'
 
+const EMPTY_PROPS = {}
+const EMPTY_PROPS_HANDLER = () => EMPTY_PROPS
+
 const computeClasses = ({ className = '', loadingRows, loadingCount }) => {
   let s = className
   if (loadingCount || loadingRows) s += ' loading'
   return s
 }
 
-const DefaultManifestTable = ({ className }) => {
+const DefaultManifestTable = ({ className, trPropsHandler = EMPTY_PROPS_HANDLER, tdPropsHandler = EMPTY_PROPS_HANDLER }) => {
   const { definition, rows, loadingRows, loadingCount } = useManifest()
   const finalClassName = computeClasses({ className, loadingRows, loadingCount })
   return (
     <>
-      <Table className={finalClassName} columnCount={definition.length} rowCount={rows.length} />
+      <Table className={finalClassName} columnCount={definition.length} rowCount={rows.length} trPropsHandler={trPropsHandler} tdPropsHandler={tdPropsHandler} />
       <Controls />
     </>
   )
 }
 
 DefaultManifestTable.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  trPropsHandler: PropTypes.func,
+  tdPropsHandler: PropTypes.func
 }
 
 export default DefaultManifestTable
