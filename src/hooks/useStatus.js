a43e9a58dd16edd3d5703e358ce030d64d999121
Charles Howard <cdhoward@add123.com>
2020-03-26 10:56:14 -0400

    Adding a loading status (#13)

--- src/hooks/useStatus.js
+++ src/hooks/useStatus.js
@@ -5,14 +5,16 @@ const determineFirstOnPage = (page, pageSize) => Math.max(page * pageSize, 0)
 const determineLastOnPage = (page, pageSize, count) => Math.min(determineFirstOnPage(page + 1, pageSize), count)
 
 export default () => {
-  const { page, pageSize, count } = useManifest()
+  const { page, pageSize, count, loadingCount, loadingRows } = useManifest()
   const lastOnPage = determineLastOnPage(page, pageSize, count)
   const firstOnPage = determineFirstOnPage(page, pageSize) + 1
+  const loading = loadingRows || loadingCount
   return {
     page,
     pageSize,
     count,
     lastOnPage,
-    firstOnPage
+    firstOnPage,
+    loading
   }
 }
