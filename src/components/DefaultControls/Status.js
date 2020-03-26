4bec89b88bfd3f0f9af230e8f418997f45dbb27f
Charles Howard <frodare@gmail.com>
2020-03-26 10:39:48 -0400

    Adding a loading status

--- src/components/DefaultControls/Status.js
+++ src/components/DefaultControls/Status.js
@@ -2,15 +2,16 @@ import React from 'react'
 import propTypes from 'prop-types'
 import useStatus from '../../hooks/useStatus'
 
-const DEFAULT_STATUS_MESSAGE_GENERATOR = ({ count, lastOnPage, firstOnPage }) => {
+const DEFAULT_STATUS_MESSAGE_GENERATOR = ({ count, lastOnPage, firstOnPage, loading }) => {
+  if (loading) return 'Loading ...'
   return count < 1 ? 'No Results' : `Showing ${firstOnPage} to ${lastOnPage} of ${count}`
 }
 
 const Status = ({ className, statusMessageGenerator = DEFAULT_STATUS_MESSAGE_GENERATOR }) => {
-  const { count, lastOnPage, firstOnPage } = useStatus()
+  const { count, lastOnPage, firstOnPage, loading } = useStatus()
   return (
     <div className={'manifest-status' + (className ? ' ' + className : '')}>
-      {statusMessageGenerator({ count, lastOnPage, firstOnPage })}
+      {statusMessageGenerator({ count, lastOnPage, firstOnPage, loading })}
     </div>
   )
 }
