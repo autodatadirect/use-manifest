d136c8a4986d650492e0639070e586c05d465737
Max Saunders <msaunders@add123.com>
2021-07-28 16:32:55 -0400

    *

--- src/components/ButtonPager.js
+++ src/components/ButtonPager.js
@@ -2,25 +2,20 @@ import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 import usePager from '../hooks/usePager'
 
-const Pager = ({ className }) =>
-  <div className={`manifest-pager ${className || ''}`} aria-label='pager'>
-    <PagerButtons />
-  </div>
-
-const PagerButtons = () => {
+const Pager = ({ className }) => {
   const { page, pages, count, pageSize, loading, showFirst, showPrevious, showNext, showLast } = usePager({ numberOfPages: 5 })
   const lastPage = Math.ceil(count / pageSize) - 1
 
   if (count != null && count < 1) return null
 
   return (
-    <>
+    <div className={`manifest-pager ${className || ''}`} aria-label='pager'>
       {showFirst ? <PagerButton page={0} loading={loading}>First</PagerButton> : null}
       {showPrevious ? <PagerButton page={page - 1} loading={loading}>{'<'}</PagerButton> : null}
       {pages.map(i => <PagerButton key={i} page={i} isCurrentPage={page === i} loading={loading}>{i + 1}</PagerButton>)}
       {showNext ? <PagerButton page={page + 1} loading={loading}>{'>'}</PagerButton> : null}
       {showLast ? <PagerButton page={lastPage} loading={loading}>Last</PagerButton> : null}
-    </>
+    </div>
   )
 }
 
