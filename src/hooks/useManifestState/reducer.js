5c8ba43a47214c3643a205bc774c1c73a84c98f0
Max Saunders <msaunders@add123.com>
2021-07-30 12:08:56 -0400

    add test for new reducer function

--- src/hooks/useManifestState/reducer.js
+++ src/hooks/useManifestState/reducer.js
@@ -65,8 +65,12 @@ const setRows = (state, action) => {
 export const correctRowCount = (state) => {
   if (!onLastPage(state)) return state
 
-  const calculatedCount = state.page * state.pageSize + state.rows.length
+  let calculatedCount = state.page * state.pageSize + state.rows.length
   if (state.count && state.count === calculatedCount) return state
+
+  if (state.pageSize === state.rows.length) {
+    calculatedCount = null
+  }
 
   return {
     ...state,
@@ -77,7 +81,7 @@ export const correctRowCount = (state) => {
 
 const onLastPage = state => {
   if (state.count === null) {
-    return state.page && state.rows.length < state.pageSize
+    return state.rows.length < state.pageSize
   }
   return pagerLogic.determineTotalPages(state.pageSize, state.count) === state.page + 1
 }
